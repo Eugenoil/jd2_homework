516c5b4a0b1a37bbee17c3be2c90ddedb50d2150
Eugene <e.dunin@bk.ru>
2021-11-16 13:53:33 +0300

    Task 2-3: fixed

--- task_hibernate/src/main/java/by/academy/it/Main.java
+++ task_hibernate/src/main/java/by/academy/it/Main.java
@@ -1,15 +1,9 @@
 package by.academy.it;
-
-
-import by.academy.it.data.PersonDao;
-import by.academy.it.pojos.Person;
 import by.academy.it.ui.Menu;
 
 
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args){
-        //Menu.getConsoleMenu();
+        Menu.getConsoleMenu();
     }
 }
